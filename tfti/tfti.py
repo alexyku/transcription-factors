ff72b08a5796805312542fc8cdf845ffab2f760d
Gunjan Baid <gunjan_baid@berkeley.edu>
2018-04-23 17:37:30 -0700

    Adding in local attention and separable convs for 1 grams.

--- tfti/tfti.py
+++ tfti/tfti.py
@@ -344,7 +344,7 @@ class DeepseaProblem(problem.Problem):
   @property
   def chunk_size(self):
     """n-gram/k-mer size."""
-    return 4
+    return 1
   
   @property
   def num_binary_predictions(self):
@@ -796,7 +796,8 @@ class TftiTransformer(transformer.Transformer):
     """
     hparams = self._hparams
 
-    hparams.ffn_layer = "conv_relu_conv"
+    hparams.ffn_layer = "conv_hidden_relu_with_sepconv"
+    hparams.self_attention_type = "local_1d"
 
     encoder_output, encoder_decoder_attention_bias = self.encode(
         inputs=features["inputs"],
@@ -806,6 +807,7 @@ class TftiTransformer(transformer.Transformer):
     # No positional embeddings on decoder side.
 
     hparams.ffn_layer = "dense_relu_dense"
+    hparams.self_attention_type = "dot_product"
 
     decoder_output = self.decode(
         decoder_input=common_layers.flatten4d3d(features["latents"]),
